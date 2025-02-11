cbfd0ef7b41700adabf21f92daaf2285656f9c27
Adeolu Rabiu <adeolu.rabiu@gmail.com>
2025-02-11 01:07:27 +0000

    Added missing book retrieval endpoint and correction to code

--- api/v1/router.py
+++ api/v1/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from api.routes import books
+from api.v1.routes import books
 
 api_router = APIRouter()
 api_router.include_router(books.router, prefix="/books", tags=["books"])
