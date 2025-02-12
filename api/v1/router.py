e20034c8863416724784801873754103901394db
Adeolu Rabiu <adeolu.rabiu@gmail.com>
2025-02-12 01:19:23 +0000

    Fix: Ensure stage2-endpoint returns 404 as expected

--- api/v1/router.py
+++ api/v1/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
-from api.v1.routes import books
+from api.v1.routes import books, stage2  # Import new route file
 
 api_router = APIRouter()
 api_router.include_router(books.router, prefix="/books", tags=["books"])
+api_router.include_router(stage2.router, prefix="/stage2", tags=["stage2"]) 
+
